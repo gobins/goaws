98043d76cc58c67fab61182fe54133bb7f553c7d
Gobin Sougrakpam <gobinsougrakpam@gmail.com>
2016-01-30 19:16:41 +1100

    Adding update-tag sub command

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ func main() {
 
 	//Defining sub-commands
 	var environment string
+	var tagname string
+	var tagvalue string
 	app.Commands = []cli.Command{
 		{
 			Name:  "get-subnets",
@@ -31,13 +33,38 @@ func main() {
 			Flags: []cli.Flag{
 				cli.StringFlag{
 					Name:        "environment",
-					Usage:       "test",
+					Usage:       "Environment Name",
 					Destination: &environment,
 				},
 			},
 			Action: func(c *cli.Context) {
 				log.Debug("Calling apihandlers.GetInstancesFormatted")
 				apihandlers.GetInstancesFormatted(environment)
+			},
+		},
+		{
+			Name:  "update-tag",
+			Usage: "Updates tag for all objects in subnet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:        "environment",
+					Usage:       "Environment Name",
+					Destination: &environment,
+				},
+				cli.StringFlag{
+					Name:        "tagname",
+					Usage:       "Key of the tag",
+					Destination: &tagname,
+				},
+				cli.StringFlag{
+					Name:        "tagvalue",
+					Usage:       "Value of the tag",
+					Destination: &tagvalue,
+				},
+			},
+			Action: func(c *cli.Context) {
+				log.Debug("Calling apihandlers.UpdateEnvTags")
+				apihandlers.UpdateEnvTags(tagname, tagvalue, environment)
 			},
 		},
 	}
